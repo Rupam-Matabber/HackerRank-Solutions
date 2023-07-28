bbe946d52cbedd0afae89e788008460f6070d213
Rupam-Matabber <rupammatabber04@gmail.com>
2023-07-28 23:04:21 +0530

    Added

---
+++ C language/printingpatterns.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    int len = (2 * n - 1), start = 0, end = (len - 1);
+    int a[len][len];
+
+    // Complete the code to print the pattern.
+    while (n != 0)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+
+                if (i == start || i == end || j == start || j == end)
+                {
+                    a[i][j] = n;
+                }
+                else{
+                    a[i][j] = n - 1;
+                }
+            }
+        }
+        start++;
+        end--;
+        n--;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
